0d818624428306ef6f3f0c7c30b1a432b75b7ad9
msk <mark.kalusha@gmail.com>
2014-02-01 03:15:10 -0600

    simpleBarCharter (sbc) version 0.0.5 - Jan 25 22:14

--- makeXRectsOfWidthW-givenListOfYRectHeights.sh
+++ makeXRectsOfWidthW-givenListOfYRectHeights.sh
@@ -11,16 +11,15 @@ baseB=5
 baseMultiplier=10
 deltaMultiplier=2
 
-plotWidth=720
-plotHeight=405
+defaultAspectRatio="16:9"
+arMultiplier=45
+#plotWidth=720
+#plotHeight=405
 rectSpacing=5
 xStartRect=10
 yStartRect=10
 
-#plotWidth=$1
-#plotHeight=$2
-rectWidth=$1 # $3
-listOfRectHeigths="$2" # "$4"
+listOfRectHeigths="$1" # "$2" # "$4"
 slideRectsToBottomOfPlot=1
 useMinimumPlotDims=1
 
@@ -31,14 +30,43 @@ yLabelStart=$yStartRect
 labelPadding=$rectSpacing
 printLabel=1
 
-if test $# -gt 2;
+if test $# -gt 1;
 	then
-		labelText="$3"
-		if test "$labelText" = ''; then printLabel=0; fi; 
+		rectWidthInput=$2 # $1 # $3
+		if test "$rectWidthInput" = '' -o "$rectWidthInput" = '0' -o "$rectWidthInput" = 'dynamic';
+			then  
+				dynamicWidthDeterminationBasedOnDefaultAspectRatio=1
+			else
+				dynamicWidthDeterminationBasedOnDefaultAspectRatio=0
+				rectWidth=$rectWidthInput
+		fi;
+		if test $# -gt 2;
+			then
+				labelText="$3"
+				if test "$labelText" = ''; then printLabel=0; fi; 
+				if test $# -gt 3;
+					then
+						plotWidth=$4 # $1
+						plotHeight=$5 # $2
+				fi;
+		fi;
+	else
+		dynamicWidthDeterminationBasedOnDefaultAspectRatio=1
+fi;
+
+if test $dynamicWidthDeterminationBasedOnDefaultAspectRatio -eq 1;
+	then
+		pi=$(echo "scale=10; 4*a(1)" | bc -l)
+		arW=`printf '%s' "$defaultAspectRatio" | cut -d ':' -f 1` 
+		arH=`printf '%s' "$defaultAspectRatio" | cut -d ':' -f 2`
+		aspectRatioA=$(echo "$arW / $arH" | bc -l)
+		aspectRatioB=$(echo "$arH / $arW" | bc -l)
 fi; 
 
+
 printf '\n\nRectangle Template: %s\n' "$rectTemplate" 
-printf '\nwidth: %i and heigths:' "$rectWidth" 
+#printf '\nwidth: %i and heigths:' "$rectWidth" 
+printf '\nRectangle Heigths:' 
 
 minPlotWidth=`expr \( $xStartRect \* 2 \) + $rectSpacing`
 minPlotHeight=`expr \( $yStartRect \* 2 \) + $rectSpacing`
@@ -69,6 +97,18 @@ if test $printLabel -eq 1;
 	else
 		labelDisplay='none'
 fi;
+
+if test $dynamicWidthDeterminationBasedOnDefaultAspectRatio -eq 1;
+	then 
+		minPlotWidthFloat=$(echo "$minPlotHeight * $aspectRatioA" | bc -l)
+		rectWidthFloat=$(echo "( $minPlotWidthFloat - ( $xStartRect * 2 ) ) / ( $numRects * 1.2 )" | bc -l)
+		rectSpacingFloat=$(echo "$rectWidthFloat * 0.2" | bc -l)
+		rectWidth=`printf '%s' "$rectWidthFloat" | cut -d '.' -f 1` #`expr $rectWidthFloat + 1`
+		rectSpacing=`printf '%s' "$rectSpacingFloat" | cut -d '.' -f 1` #`expr $rectSpacingFloat - 1`
+		printf '\nminPlotWidthFloat (%s) and rectWidthFloat (%s) and rectSpacingFloat (%s) dynamically determined based on defaultAspectRatio (%s) and maxY (%s).\n' "$minPlotWidthFloat" "$rectWidthFloat" "$rectSpacingFloat" "$defaultAspectRatio" "$maxY"
+fi;
+
+printf '\nRectangle Widths (all equal): %i' "$rectWidth"
 minPlotWidth=`expr \( \( $rectWidth + $rectSpacing \) \* $numRects \) + \( $xStartRect \* 2 \) - $rectSpacing`
 
 if test $useMinimumPlotDims -eq 1;
